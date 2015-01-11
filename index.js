c33f7c831b40a0f1d37a45b82b268a2ad36e4f84
xiongxoy <xiong.xoy@gmail.com>
2015-01-11 16:49:42 +0800

    Fix Bug and Refactor Code
    http://bbs.besike.com/t/lesson-9-express/397/10

--- index.js
+++ index.js
@@ -4,7 +4,21 @@ var http = require('http');
 
 function myexpress() {
 
-  var app = function (request, response) {
+  var app = function (request, response, next2) {
+    var stackIdx;
+
+    (function startApp() {
+      if (app.stack && app.stack[0]) { // app not empty
+        stackIndex = 0; // index for stack
+        try {
+          next();
+        } catch(error) {
+          end(500, error);
+        }
+      } else {                        // app is empty
+        end(404);
+      }
+    })();
 
     function next(error) {
       if (!error) {
@@ -17,67 +31,39 @@ function myexpress() {
     // end request
     function end(code, error) {
       if (code == 500) {
-        if (app.next2) {
-          app.next2(error);
+        if (next2) {
+          next2(error);
         }
         response.statusCode = code;
         response.end();
       } else if (code == 404) {
-        if (app.next2) {
-          app.next2();
+        if (next2) {
+          next2();
         }
         response.statusCode = code;
         response.end("404 - Not Found");
       }
     }
 
-    if (app.stack && app.stack[0]) { // app not empty
-      next.index = 0; // index for stack
-      try {
-        next();
-      } catch(error) {
-        end(500, error);
+    function callWithoutError(next) {
+      for (; stackIndex < app.stack.length; stackIndex++) {
+        if (app.stack[stackIndex].length == 3) {
+          stackIndex += 1;
+          app.stack[stackIndex-1](request, response, next);
+        }
       }
-    } else { // app is empty
       end(404);
     }
 
-    function callWithoutError(next) {
-      if (next.index == app.stack.length) {
-        end(404);
-        return;
-      } else {
-        // find a middleware with no error parameter
-        while(app.stack[next.index].length != 3) {
-          next.index += 1;
-          if (next.index == app.stack.length) {
-            end(404);
-            return;
-          }
-        }
-        next.index += 1;
-        app.stack[next.index-1](request, response, next);
-      }
-    }
-
     function callWithError(next, error) {
-      if (next.index == app.stack.length) {
-        end(500, error)
-        return;
-      } else {
-        // find a middleware with error parameter
-        while(app.stack[next.index].length != 4) {
-          next.index += 1;
-          if (next.index == app.stack.length) {
-            end(500, error)
-            return;
-          }
+      for (; stackIndex < app.stack.length; stackIndex++) {
+        if (app.stack[stackIndex].length == 4) {
+          stackIndex += 1;
+          app.stack[stackIndex-1](error, request, response, next);
         }
-        next.index += 1;
-        app.stack[next.index-1](error, request, response, next);
       }
+      end(500, error)
     }
-
   } // end of function app(request, response);
 
   app.listen = function (port, done) {
@@ -88,22 +74,7 @@ function myexpress() {
 
   app.use = function (middleware) {
     app.stack = app.stack || [];
-
-    if (!middleware.use) {
-      app.stack.push(middleware);
-    } else  { // middleware is an app
-      var embeddingMiddleware = createEmbeddingMiddleware(middleware);
-      app.stack.push(embeddingMiddleware);
-    }
-  }
-
-  function createEmbeddingMiddleware(subApp) {
-    var embeddingMiddleware = function (request, response, next) {
-      // start inner middleware
-      subApp.next2 = next;
-      subApp(request, response);
-    }
-    return embeddingMiddleware;
+    app.stack.push(middleware);
   }
 
   return app;
