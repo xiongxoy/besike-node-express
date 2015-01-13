7215fdcd51205a111c0f32b46caa8310a22b3755
xiongxoy <xiong.xoy@gmail.com>
2015-01-13 20:25:22 +0800

    Implement Prefix Path Trimming

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ function express() {
     'use strict';
 
     var app = function (request, response, next2) {
+      app.handle(request, response, next2);
+    };
+
+    app.handle = function (request, response, next2) {
 
         var stackIndex;
         (function startApp() {
@@ -21,63 +25,76 @@ function express() {
         }());
 
         function next(error) {
-            if (!error) {
-                callWithoutError(next);
-            } else {
-                callWithError(next, error);
+          var m, target;
+          for (; stackIndex < app.stack.length; stackIndex++) {
+            target = app.stack[stackIndex];
+            if (isSame(isErrorHandler(target.handle), error) && (m = target.match(request.url))) {
+              request.params = m.params;
+              stackIndex++;
+              // trim url
+              if (isApp(target.handle)) {
+                request.oldUrl = request.url;
+                request.url = request.url.slice(m.path.length);
+                request.url = request.url[0] === '/' ? request.url : '/' + request.url;
+              }
+              // call handle
+              if (error) {
+                target.handle(error, request, response, next);
+              } else {
+                target.handle(request, response, next);
+              }
+              return;
             }
+          }
+          if (error) {
+            end(500, error);
+          } else {
+            end(404);
+          }
         }
 
         // end request
         function end(code, error) {
-            if (code === 500) {
-                if (next2) {
-                    next2(error);
-                    return;
-                }
-                response.statusCode = code;
-                response.end();
-            } else if (code === 404) {
-                if (next2) {
-                    next2();
-                    return;
-                }
-                response.statusCode = code;
-                response.end("404 - Not Found");
+          if (code === 500) {
+            if (next2) {
+              if (request.oldUrl) {
+              }
+              if (request.oldurl) {
+                request.url = request.oldUrl;
+                delete request.oldUrl;
+              }
+              next2(error);
+              return;
             }
+            response.statusCode = code;
+            response.end();
+          } else if (code === 404) {
+            if (next2) {
+              if (request.oldUrl) {
+                request.url = request.oldUrl;
+                delete request.oldUrl;
+              }
+              next2();
+              return;
+            }
+            response.statusCode = code;
+            response.end("404 - Not Found");
+          }
         }
 
-        function callWithoutError(next) {
-            var m;
-            for (; stackIndex < app.stack.length; stackIndex++) {
-                if (!isErrorHandler(app.stack[stackIndex].handle) &&
-                    (m = app.stack[stackIndex].match(request.url))) {
-                    request.params = m.params;
-                    app.stack[stackIndex++].handle(request, response, next);
-                    return;
-                }
-            }
-            end(404);
+        function isSame(a, b) {
+          return Boolean(a) == Boolean(b);
         }
 
-        function callWithError(next, error) {
-            var m;
-            for (; stackIndex < app.stack.length; stackIndex++) {
-                if (isErrorHandler(app.stack[stackIndex].handle) &&
-                    (m = app.stack[stackIndex].match(request.url))) {
-                    request.params = m.params;
-                    app.stack[stackIndex++].handle(error, request, response, next);
-                    return;
-                }
-            }
-            end(500, error);
+        function isApp(handle) {
+            return typeof handle.handle === 'function';
         }
 
         function isErrorHandler(fn) {
             return fn.length >= 4;
         }
 
-    }; // end of function app(request, response, next2);
+    }; // end of handle function (request, response, next2);
 
     app.stack = [];
 
